4690aaa7f56e3665b641799f50bcfb45f08acb5b
CODEALEJO <salomee579@gmail.com>
2025-09-25 16:15:17 -0500

    primercommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RealtechOrden.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
